24b1aed629ac55f8fce04a1e7981be87a9528574
sagarv1997 <sagarv1997@gmail.com>
2019-03-24 14:00:06 +0530

    Profile UI

--- components/Screens/Profile.js
+++ components/Screens/Profile.js
@@ -6,9 +6,10 @@ import {
   FlatList,
   Image,
   ScrollView,
+  StyleSheet,
   ActivityIndicator
 } from "react-native";
-import { TextInput, Button } from "react-native-paper";
+import { Appbar } from "react-native-paper";
 import FullWidthImage from "../CustomLibrary/FullWidthImage";
 import firebase from "react-native-firebase";
 const CommentsEmpty = require("../../src/images/comments_empty.png");
@@ -52,9 +53,7 @@ export default class Profile extends Component {
   _ListEmptyComponent = (
     <View>
       <View elevation={3} style={{ padding: 5 }}>
-        <Text style={{ textAlign: "center" }}>
-          Oh my, no uploads or posts here!
-        </Text>
+        <Text style={{ textAlign: "center" }}>User is not active!</Text>
         <Image
           source={CommentsEmpty}
           style={{ height: 40, width: 40, alignSelf: "center" }}
@@ -64,14 +63,28 @@ export default class Profile extends Component {
   );
 
   _renderItem = ({ item }) => (
-    <View>
+    <View
+      style={{ padding: 1, justifyContent: "center", alignItems: "center" }}
+    >
       <Image
         source={item.image == "" ? loading_gif : { uri: item.image }}
-        style={{ height: 150, width: 150 }}
+        style={{ height: 132, width: 132 }}
         resizeMode="cover"
       />
     </View>
   );
+
+  getProfileText(name) {
+    n = name;
+    return n.charAt(0);
+  }
+
+  getProfileBackground() {
+    Colors = ["black", "#673AB7", "#3F51B5", "#FFC107", "#607D8B", "#4CAF50"];
+    ColorNumber = Math.floor(Math.random() * 6);
+    return Colors[ColorNumber];
+  }
+
   render() {
     console.log(this.state.pictures);
     console.log(this.user);
@@ -96,6 +109,85 @@ export default class Profile extends Component {
         />
       );
     }
-    return <View>{ListOfPictures}</View>;
+    return (
+      <View style={styles.container}>
+        <Appbar.Header style={{ backgroundColor: "#009688" }}>
+          <Appbar.Action
+            icon="arrow-back"
+            color="white"
+            onPress={() => this.props.navigation.goBack()}
+          />
+          <Appbar.Content title={this.user} color="white" />
+        </Appbar.Header>
+        <View
+          style={{
+            backgroundColor: "#fff",
+            flexDirection: "row"
+          }}
+        >
+          <View style={{ width: "30%", padding: 20 }}>
+            <View
+              style={{
+                height: 100,
+                width: 100,
+                borderRadius: 50,
+                backgroundColor: this.getProfileBackground(),
+                justifyContent: "center",
+                alignItems: "center"
+              }}
+            >
+              <Text style={{ fontSize: 50, color: "#fff", fontWeight: "bold" }}>
+                {this.getProfileText(this.user)}
+              </Text>
+            </View>
+          </View>
+          <View
+            style={{
+              width: "70%",
+              justifyContent: "center",
+              alignItems: "center"
+            }}
+          >
+            <View>
+              <Text
+                style={{
+                  fontSize: 70,
+                  color: "black",
+                  fontWeight: "bold"
+                }}
+              >
+                {this.state.pictures.length}
+              </Text>
+            </View>
+            <View>
+              <Text
+                style={{ fontWeight: "bold", fontSize: 18, color: "black" }}
+              >
+                Posts
+              </Text>
+            </View>
+          </View>
+        </View>
+        {/* <View style={{ backgroundColor: "#fff", paddingHorizontal: 25 }}>
+          <Text style={{ fontWeight: "bold", color: "black", fontSize: 20 }}>
+            {this.user}
+          </Text>
+        </View> */}
+        <View style={{ padding: 1, backgroundColor: "#fff" }} />
+        <View style={{ padding: 1, backgroundColor: "#EDEEF3" }} />
+
+        {ListOfPictures}
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  LineBorder: {
+    height: 4
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
+  }
+});
